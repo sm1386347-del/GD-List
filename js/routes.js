362caba82634801d866d2d202d07d6fc81dc54bb
sm1386347-del <sm1386347@gmail.com>
2026-09-05 22:28:03 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -3,7 +3,7 @@ import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
-    { path: '/', component: List },
-    { path: '/leaderboard', component: Leaderboard },
-    { path: '/roulette', component: Roulette },
+    { path: '/GD-List/', component: List },
+    { path: '/GD-List/leaderboard', component: Leaderboard },
+    { path: '/GD-List/roulette', component: Roulette },
 ];
