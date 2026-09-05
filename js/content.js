627dcdafa1f7876c77326a8ac2b4cc894cf4dc35
sm1386347-del <sm1386347@gmail.com>
2026-09-05 23:26:27 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@ import { round, score } from './score.js';
 /**
  * Path to directory containing `_list.json` and all levels
  */
-const dir = 'data';
+const dir = new URL('./data', import.meta.url).href;
 export async function fetchList() {
    const listResult = await fetch(`${dir}/_list.json`);
     try {
