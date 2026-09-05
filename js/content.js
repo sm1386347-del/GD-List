8c0418de380c174369616e05280cfa6f2724c6bf
sm1386347-del <sm1386347@gmail.com>
2026-09-05 23:41:01 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -14,3 +14,8 @@ export async function fetchEditors() {
     const editorsResults = await fetch(`${dir}/_editors.json`);
     return await editorsResults.json();
 }
+
+export async function fetchLeaderboard() {
+    const leaderboardResult = await fetch(`${dir}/_leaderboard.json`);
+    return await leaderboardResult.json();
+}
