d052fa70845424e724d3c460f942e0c2cf83b3e0
sm1386347-del <sm1386347@gmail.com>
2026-09-06 00:14:35 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,7 +1,25 @@
 export const store = {
-    // Score/Store State
+    dark: false,
 };
 
-export const score = store;
+export function score(rank, outOf, percent) {
+    if (rank > outOf) {
+        return 0;
+    }
+    let score = round(
+        (toScore(percent) / toScore(100)) * 
+        ((outOf - rank + 1) / outOf) * 
+        50
+    );
+    return Math.max(0, score);
+}
+
+function toScore(percent) {
+    return Math.floor((percent ** 3) / 10000);
+}
+
+function round(num) {
+    return Math.round(num * 10) / 10;
+}
 
 export default store;
