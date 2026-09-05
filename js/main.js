6f4382dbf625c4f38131314c1e802ca5ecd4d2aa
sm1386347-del <sm1386347@gmail.com>
2026-09-05 23:50:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
 import routes from './routes.js';
+import store from './score.js';
 
-export const store = Vue.reactive({
-    dark: JSON.parse(localStorage.getItem('dark')) || false,
-    toggleDark() {
-        this.dark = !this.dark;
-        localStorage.setItem('dark', JSON.stringify(this.dark));
-    },
-});
-
-const app = Vue.createApp({
-    data: () => ({ store }),
-});
 const router = VueRouter.createRouter({
     history: VueRouter.createWebHashHistory(),
     routes,
 });
 
-app.use(router);
+const app = Vue.createApp({
+    data() {
+        return {
+            store,
+        };
+    },
+});
 
+app.use(router);
 app.mount('#app');
