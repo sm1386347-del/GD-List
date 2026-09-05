f0c16417a5f2e332c67f3a8c2b39027622c5591b
sm1386347-del <sm1386347@gmail.com>
2026-09-05 23:30:11 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -3,14 +3,14 @@ import { round, score } from './score.js';
 /**
  * Path to directory containing `_list.json` and all levels
  */
-const dir = new URL('./data', import.meta.url).href;
+const dir = 'data';
 export async function fetchList() {
-  const listResult = await fetch(`${dir}_list.json`);
+ const listResult = await fetch(./${dir}/_list.json);
     try {
         const list = await listResult.json();
         return await Promise.all(
             list.map(async (path, rank) => {
-               const levelResult = await fetch(`${dir}${path}.json`);
+               const levelResult = await fetch(./${dir}/${path}.json);
                 try {
                     const level = await levelResult.json();
                     return [
@@ -37,7 +37,7 @@ export async function fetchList() {
 
 export async function fetchEditors() {
     try {
-       const editorsResults = await fetch(`${dir}_editors.json`);
+       const editorsResults = await fetch(./${dir}/_editors.json);
         const editors = await editorsResults.json();
         return editors;
     } catch {
