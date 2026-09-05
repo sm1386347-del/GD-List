c85a36e2e83121b3b3bbb50094371cd41baafe6a
sm1386347-del <sm1386347@gmail.com>
2026-09-06 00:04:39 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -1,6 +1,6 @@
 import { store } from "../main.js";
 import { embed } from "../util.js";
-import { score } from "../score.js";
+import store from '../score.js?v=8';
 import { fetchEditors, fetchList } from "../content.js";
 
 import Spinner from "../components/Spinner.js";
